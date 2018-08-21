ba08b8395d5b27c397c6f3bf29d43e7c0ace13e3
'rmlb' <'bluelibra001@163.com'>
2018-08-21 15:52:12 +0800

    抽象方法添加

--- src/main/java/com/zhiyou100/rmlb/crm/dao/AbstractDAO.java
+++ src/main/java/com/zhiyou100/rmlb/crm/dao/AbstractDAO.java
@@ -1,5 +1,14 @@
 package com.zhiyou100.rmlb.crm.dao;
 
-public abstract class AbstractDAO {
+public abstract class AbstractDAO<K, V> {
 
+	
+	abstract boolean add(V vo);
+	
+	abstract boolean delete(K id);
+	
+	abstract boolean update(V vo);
+	
+	abstract V findById(K id);
+	
 }
